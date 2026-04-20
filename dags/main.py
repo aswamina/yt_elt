c6d2f5ee36f73761b0f40a2eb866bbece0dd601a
Anand Swaminathan <aswamina@gmail.com>
2026-04-20 12:21:02 -0500

    added functional, unit and integration tests. Also added data triggers for dags

--- dags/main.py
+++ dags/main.py
@@ -4,7 +4,8 @@ from datetime import date, datetime, timedelta
 from api.video_stats import get_channel_playlist_id, get_video_ids, save_to_json, get_video_details
 from airflow.models import Variable
 from datawarehouse.dwh import staging_table, core_table
-
+from dataquality.soda import yt_elt_data_quality
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
 YOUTUBE_API_KEY=Variable.get("YOUTUBE_API_KEY")
@@ -23,6 +24,10 @@ default_args = {
     'start_date': pendulum.datetime(2026, 4, 12, tz="UTC"),
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
+
 with DAG(
     dag_id='youtube_channel_video_stats',
     default_args=default_args,
@@ -38,8 +43,13 @@ with DAG(
     video_details = get_video_details(video_ids, YOUTUBE_API_KEY)
     save_to_json_task = save_to_json(video_details, f"data/video_details_{date.today()}.json")
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
     # Task dependencies
-    playlist_id >> video_ids >> video_details >> save_to_json_task
+    playlist_id >> video_ids >> video_details >> save_to_json_task >> trigger_update_db
     
 
 # DAG 2: update_db
@@ -55,5 +65,28 @@ with DAG(
     update_staging = staging_table()
     update_core = core_table()
 
+
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # Define dependencies
-    update_staging >> update_core 
+    update_staging >> update_core >> trigger_data_quality
+
+
+# DAG 3: data_quality
+with DAG(
+    dag_id="data_quality",
+    default_args=default_args,
+    description="DAG to check the data quality on both layers in the database",
+    catchup=False,
+    schedule=None,
+) as dag_quality:
+
+    # Define tasks
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    # Define dependencies
+    soda_validate_staging >> soda_validate_core
